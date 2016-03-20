e513a8323e7ac6a4aa84432bf9847185ea8f47a3
jakecoltman <jakecoltman@sky.com>
2016-03-20 09:35:39 +0000

    Added a piece listener to encapsualte the message handling
    Listener takes a piece as an argument and then controls it accordingly.
    Nice touch is interface design, one piece can be assigned to multiple
    listeners which only use sub sections of its interface

---
+++ Listeners/PieceListener.py
@@ -0,0 +1,31 @@
+from Events.MovementEvent import MovementEvent
+from Events.RollbackEvent import RollBackEvent
+
+class PieceListener:
+
+    def __init__(self, piece):
+        self.piece = piece
+        self.message_history = []
+        self.invalid_ids = []
+
+    def recieve_message(self, message):
+        if type(message) == RollBackEvent:
+            self.invalid_ids.append(message.id)
+            self.rollback_to_message_id(message.id)
+
+        if message.id in self.invalid_ids:
+            return
+        self.message_history.append(message)
+        self.piece.update()
+
+    def rollback_to_message_id(self, rollback_id):
+        complete = False
+        if rollback_id not in [x.id for x in self.message_history]:
+            return
+        while not complete:
+            rollingBackMessage = self.message_history.pop()
+            if rollingBackMessage.id == rollback_id:
+                complete = True
+            #self.pos = rollingBackMessage.from_pos
+        return True
+
