208107b365efa43f2070d6c65816e4e0f6ec32f1
jakecoltman <jakecoltman@sky.com>
2016-03-20 10:19:00 +0000

    Added game state enum
    Allows representation of whether the game is not started, going or gone

--- Game.py
+++ Game.py
@@ -1,9 +1,15 @@
 from enum import Enum
 
 
-class Turn(Enum):
+class Sides(Enum):
     white = 0
     black = 1
+
+
+class GameState(Enum):
+    not_started = 0
+    in_progress = 1
+    finished = 2
 
 
 class Game:
