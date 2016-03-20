3974dc67368ba5eb7669c1ebbb3e808e3c279bd0
jakecoltman <jakecoltman@sky.com>
2016-03-20 10:25:52 +0000

    Built out game class
    Holds the current turn and state

--- Game.py
+++ Game.py
@@ -1,3 +1,16 @@
+from DomainModel.Sides import Sides
+from DomainModel.GameState import GameState
+
+
 class Game:
     def __init__(self):
-        self.turn = "White"
+        self.turn = Sides(0)
+        self.state = GameState(0)
+        self.winning_side = None
+
+    def start_game(self):
+        self.state = GameState.in_progress
+
+    def finish_game(self, winning_side: Sides):
+        self.winning_side = winning_side
+        self.state = GameState.finished
