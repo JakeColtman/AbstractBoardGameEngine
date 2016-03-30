8fe167bf80b9157a981f52896a8d7d24efd7c831
jakecoltman <jakecoltman@sky.com>
2016-03-30 19:36:27 +0100

    Working towards f# message bus

---
+++ V2/AbstractBoardGameEngine/AbstractBoardGameEngine/MessageBus.fs
@@ -0,0 +1,29 @@
+﻿namespace ABGE
+#nowarn "40"
+
+module Messages = 
+    type Issuer = 
+        | User of username: string
+
+    type Message = {Content: obj; SagaId: int; Issuer: Issuer}
+
+    type MessageHandler = string -> Unit
+
+    let print_message (message:string) = printfn "%s" message
+    let print_message_again (message:string) = printfn "%s" message
+
+    let MB2 process_message = 
+        MailboxProcessor.Start(fun inbox ->
+
+            let rec loop = async {
+                let! msg = inbox.Receive()
+                process_message msg
+                return! loop
+                }
+            loop)
+
+    let create_message_bus (handlers:list<MessageHandler>) = 
+        let processing_function (handlers:list<MessageHandler>) (message:string) = 
+            handlers |> List.iter(fun handler -> handler message)
+
+        MB2 (processing_function handlers)
---
+++ V2/AbstractBoardGameEngine/AbstractBoardGameEngine/Program.fs
@@ -0,0 +1,9 @@
+﻿
+
+[<EntryPoint>]
+let main argv = 
+    let handlers = [ABGE.Messages.print_message; ABGE.Messages.print_message_again]
+    let bus = ABGE.Messages.create_message_bus handlers
+    bus.Post "Hello"
+    System.Console.Read() |> ignore
+    0 // return an integer exit code
