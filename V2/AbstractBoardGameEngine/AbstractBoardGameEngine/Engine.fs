8ff7212bf3c20f56b39a1bb79a0eb5a17dc3b9aa
jakecoltman <jakecoltman@sky.com>
2016-03-31 18:45:31 +0100

    Playing with tic tac toe
    https://fsharpforfunandprofit.com/posts/enterprise-tic-tac-toe/

---
+++ V2/AbstractBoardGameEngine/AbstractBoardGameEngine/Engine.fs
@@ -0,0 +1,51 @@
+﻿namespace ABGE
+
+module TicTacToeDomain = 
+
+    type HorizPosition = Left | HCenter | Right
+    type VertPosition = Left | VCenter | Right
+
+    type CellPosition = HorizPosition * VertPosition
+
+    type Player = Player0 | PlayerX
+
+    type PlayerXPos = PlayerXPos of CellPosition
+    type PlayerYPos = PlayerYPos of CellPosition
+
+    type CellState = 
+        | Played of Player
+        | Empty
+
+    type Cell = {
+        pos : CellPosition
+        state: CellState
+    }
+        
+    type GameState = {
+        cells: Cell list
+    }
+
+    type GameStatus = 
+        | InProgress
+        | PlayerXWon
+        | PlayerOWon
+        | Draw
+
+    type ValidMovesForPlayerX = PlayerXPos list
+    type ValidMovesForPlayerO = PlayerYPos list
+
+    type MoveResult = 
+        | PlayerXToMove of GameState * ValidMovesForPlayerX
+        | PlayerOToMove of GameState * ValidMovesForPlayerO
+        | GameWon of GameState * Player
+        | GameDrawn of GameState
+
+    type PlayerXMoves = GameState * PlayerXPos -> 
+                            GameState * MoveResult
+    type PlayerOMoves = GameState * PlayerYPos -> 
+                            GameState * MoveResult
+
+    type GetCells = GameState -> Cell list
+
+    type NewGame = GameState * MoveResult
+
--- V2/AbstractBoardGameEngine/AbstractBoardGameEngine/Program.fs
+++ V2/AbstractBoardGameEngine/AbstractBoardGameEngine/Program.fs
@@ -2,10 +2,10 @@
 
 [<EntryPoint>]
 let main argv = 
-    let handlers = [ABGE.Messages.print_message; ABGE.Messages.print_message_again]
-    let factory = ABGE.Messages.MessageBusFactory handlers
-    let bus = factory.create_message_bus
-    bus.Post (ABGE.Messages.Message.CreateMessage("Hello Jake", 1))
+//    let handlers = [ABGE.Messages.print_message; ABGE.Messages.print_message_again]
+//    let factory = ABGE.Messages.MessageBusFactory handlers
+//    let bus = factory.create_message_bus
+//    bus.Post (ABGE.Messages.Message.CreateMessage("Hello Jake", 1))
 
     
 
